c9c5b2d6a524d7672811859e7d6f789ad512bd14
Arthur Rodrigues <117749096+arthurrodrigues-dev@users.noreply.github.com>
2023-07-13 22:12:12 -0300

    bee 1238 - combinator

--- string_challenges/1238.py
+++ string_challenges/1238.py
@@ -1,14 +1,15 @@
 N = int(input())
 
-
 for i in range(N):
     str1, str2 = input().split()
-    res = ""
+    answer = ""
+
     i = 0
     while i < len(str1) + len(str2):
         if (i < len(str1)):
-            res += str1[i]
+            answer += str1[i]
         if (i < len(str2)):
-            res += str2[i]
+            answer += str2[i]
         i += 1
-    print(res)
+
+    print(answer)
